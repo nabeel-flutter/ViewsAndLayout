f10b2baac99160215d7d982d3ca9e1ee5ab360c7
Muhammad Nbbeel <nabeelshakeel966@gmail.com>
2025-05-04 03:12:34 +0500

    Add initial asset and update ContentView layout

--- ViewsAndLayout/ContentView.swift
+++ ViewsAndLayout/ContentView.swift
@@ -9,55 +9,37 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    @State var isOn = false
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+        VStack{
+            Text("Hello")
+            Text("World")
+            HStack{
+                Image(systemName: "person.fill")
+                Text("Picture")
+            }
+            Toggle("Toggle",isOn: $isOn)
+            ZStack(
+                alignment: .top
+            ){
+                Rectangle()
+                VStack(alignment: .trailing){
+                    Text(
+                        "On Top of Rectangle"
+                    ).foregroundStyle(Color.white)
+                    HStack(
+                        alignment: .top,
+                        spacing: 0){
+                        Image("apple")
+                        Text("Apple").foregroundStyle(Color.white)
+                        
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-#if os(macOS)
-            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-#endif
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                    
                 }
             }
-        } detail: {
-            Text("Select an item")
         }
-    }
+      }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
 }
 
 #Preview {
