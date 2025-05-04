d5886ff044a0b02c7759c5d4e32fb08bdee2f0d9
Muhammad Nbbeel <nabeelshakeel966@gmail.com>
2025-05-04 14:07:53 +0500

    Refactor ContentView layout to use GeometryReader for responsive design

--- ViewsAndLayout/ContentView.swift
+++ ViewsAndLayout/ContentView.swift
@@ -11,33 +11,55 @@ import SwiftData
 struct ContentView: View {
     @State var isOn = false
     var body: some View {
-        VStack{
-            Text("Hello")
-            Text("World")
-            HStack{
-                Image(systemName: "person.fill")
-                Text("Picture")
-            }
-            Toggle("Toggle",isOn: $isOn)
-            ZStack(
-                alignment: .top
-            ){
-                Rectangle()
-                VStack(alignment: .trailing){
-                    Text(
-                        "On Top of Rectangle"
-                    ).foregroundStyle(Color.white)
-                    HStack(
-                        alignment: .top,
-                        spacing: 0){
-                        Image("apple")
-                        Text("Apple").foregroundStyle(Color.white)
+//        VStack{
+            
+//            Text("Hello World").offset(x:-50,y:50).border(Color.black)
+            GeometryReader(content: {
+                geometry in VStack{
+                    HStack{
+              
+                              Image("apple")
+                                .frame(width : geometry.size.width/2).border(Color.black)
+                              Text("apple")   .frame(width : geometry.size.width/2).border(Color.black)
                         
-                    }
-                    
-                }
-            }
-        }
+                    }.padding(.horizontal, 10)
+                    Spacer()
+                }.frame(width: geometry.size.width).border(Color.black)
+            })
+//            HStack{
+//                
+//                Image("apple"
+//                ).border(Color.black)
+//                Text("apple").border(Color.black)
+//                
+//            }.padding([.top, .leading,], 40)
+//            Spacer()
+            //            HStack{
+            //                Image(systemName: "person.fill")
+            //                Text("Picture")
+            //            }
+            //            Toggle("Toggle",isOn: $isOn)
+            //            ZStack(
+            //                alignment: .top
+            //            ){
+            //                Rectangle()
+            //                VStack(alignment: .trailing){
+            //                    Text(
+            //                        "On Top of Rectangle"
+            //                    ).foregroundStyle(Color.white)
+            //                    HStack(
+            //                        alignment: .top,
+            //                        spacing: 0){
+            //                        Image("apple")
+            //                            Spacer()
+            //                        Text("Apple").foregroundStyle(Color.white)
+            //
+            //                    }
+            //
+            //                }
+            //            }
+            //        }.padding(.vertical, 100)
+//        }
       }
 
 }
